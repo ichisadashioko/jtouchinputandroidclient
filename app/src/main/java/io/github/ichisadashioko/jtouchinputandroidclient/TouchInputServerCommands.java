87e0eec3780e0e6ff98da89de588fb4cc786e777
shioko <ichisadashioko@gmail.com>
2020-12-23 07:55:48 +0000

    add constants from the server project

---
+++ app/src/main/java/io/github/ichisadashioko/jtouchinputandroidclient/TouchInputServerCommands.java
@@ -0,0 +1,7 @@
+package io.github.ichisadashioko.jtouchinputandroidclient;
+
+public class TouchInputServerCommands {
+    public static final byte STOP = 0;
+    public static final byte START = 1;
+    public static final byte PAUSE = 2;
+}
