97f161736f62edc02b977704d54940d758f70ded
shioko <ichisadashioko@gmail.com>
2020-12-23 10:23:50 +0000

    add scaffolding code for handling server commands

---
+++ app/src/main/java/io/github/ichisadashioko/jtouchinputandroidclient/DefaultTouchInputCommandsListener.java
@@ -0,0 +1,7 @@
+package io.github.ichisadashioko.jtouchinputandroidclient;
+
+public class DefaultTouchInputCommandsListener implements TouchInputCommandsListener{
+    public void start(){}
+    public void pause(){}
+    public void stop(){}
+}
---
+++ app/src/main/java/io/github/ichisadashioko/jtouchinputandroidclient/TouchInputCommandsListener.java
@@ -0,0 +1,7 @@
+package io.github.ichisadashioko.jtouchinputandroidclient;
+
+public interface TouchInputCommandsListener {
+    void start();
+    void pause();
+    void stop();
+}
